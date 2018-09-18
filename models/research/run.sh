67fc9a28de771dc8c6d4d8fb795c90e80d33cf46
sujianyu <sujianyu@foxmail.com>
2018-09-18 21:24:04 +0800

    Update run.sh

--- models/research/run.sh
+++ models/research/run.sh
@@ -35,12 +35,12 @@ do
     sed -i "s/^  num_steps: $last$/  num_steps: $current/g" $pipeline_config_path  # 通过num_steps控制一次训练最多100step
 
     echo "############" $i "training #################"
-    python3 ./object_detection/train.py \
+    python ./object_detection/train.py \
     --train_dir=$train_dir \
     --pipeline_config_path=$pipeline_config_path
 
     echo "############" $i "evaluating, this takes a long while #################"
-    python3 ./object_detection/eval.py 
+    python ./object_detection/eval.py 
     --checkpoint_dir=$checkpoint_dir \
     --eval_dir=$eval_dir \
     --pipeline_config_path=$pipeline_config_path
@@ -50,19 +50,19 @@ done
 echo "############ runnning #################"
 echo "############training 2000 steps #################"
 sed -i "s/^  num_steps: $last$/  num_steps: 2000/g" $pipeline_config_path
-python3 ./object_detection/train.py \
+python ./object_detection/train.py \
     --train_dir=$train_dir \
     --pipeline_config_path=$pipeline_config_path
     
 echo "############ evaluating, this takes a long while #################"
-python3 ./object_detection/eval.py 
+python ./object_detection/eval.py 
 	--logtostderr \
     --checkpoint_dir=$checkpoint_dir \
     --eval_dir=$eval_dir \
     --pipeline_config_path=$pipeline_config_path
 
 # 导出模型
-python3 ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-$current  --output_directory $output_dir/exported_graphs
+python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-$current  --output_directory $output_dir/exported_graphs
 
 # 在test.jpg上验证导出的模型
-python3 ./inference.py --output_dir=$output_dir --dataset_dir=$dataset_dir
+python ./inference.py --output_dir=$output_dir --dataset_dir=$dataset_dir
